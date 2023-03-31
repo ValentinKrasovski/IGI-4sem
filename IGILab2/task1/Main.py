441f543939bbe026309869fd6f9ab6e61363ee41
ValentinKrasovski <valentinkrasovski8@gmail.com>
2023-03-31 19:20:57 +0300

    add task1

---
+++ IGILab2/task1/Main.py
@@ -0,0 +1,16 @@
+import Utilities
+import os
+
+path_for_folder = "/home/valentin/Studing/IGI/IGILab2/task1"
+file_name = "file.txt"
+full_path = os.path.join(path_for_folder, file_name)
+
+
+if(Utilities.check_file(full_path)):
+    file = open(full_path, encoding="UTF8")
+    text = file.read();
+    print(f"Total number of sentences {Utilities.total_number_sentences(text)}")
+    print(f"Total number of non declarative sentences {Utilities.count_nondeclarative_sentences(text)}")
+    print(f"Average length words {Utilities.average_len_words(text)}")
+    print(f"Average length sentensec {Utilities.average_len_sentences(text)}")
+    print(f"Top-k N-grams{Utilities.top_repeted_grams(text)}")
