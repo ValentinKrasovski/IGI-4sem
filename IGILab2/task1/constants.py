d1cf6a831d1ec7c3fc0a8728582a865bd10010f1
ValentinKrasovski <valentinkrasovski8@gmail.com>
2023-04-10 22:13:41 +0300

    add constants

---
+++ IGILab2/task1/constants.py
@@ -0,0 +1,8 @@
+list_of_single_abbreviations = ["etc.", "vs.", "Jr.", "Sr.", "Dr.", "Smth.", "Smb.", "Mr.", "Mrs."]
+list_of_double_abbreviations = ["e.g.", "i.e.", "a.m.", "p.m.", "P.S."]
+list_of_triple_abbreviations = ["V.I.P.", "M.V.P.", "P.S.S."]
+check_file_reg = r'[А-я]'
+all_sentences_reg = r"(\.+|\!+\?+|\?+\!+|\?+|\!+)"
+non_dec_sentences_reg = r'\!+\?+|\?+\!+|\?+|\!+'
+words_amount_reg = r'\b\w+\b'
+nums_amount_reg = r'\b\d+\b'
